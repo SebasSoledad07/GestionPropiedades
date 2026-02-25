e7d9f63cd8126674d06915e905b86a0d3b1acd14
SebasSoledad07 <sebastiansg@ufps.edu.co>
2026-02-24 20:40:42 -0400

    proyecto creado y estructurado

---
+++ src/test/java/com/example/gestionpropiedades/GestionPropiedadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionpropiedades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPropiedadesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
